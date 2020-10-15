4f2d214f4b0dea37a9ef2dd2ae99f89acbcd7e5f
sin471 <tachiken.0414.ken@gmail.com>
2020-10-15 16:48:30 +0900

    todoの追加

--- crawler_GUI.py
+++ crawler_GUI.py
@@ -1,23 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 from icrawler.builtin import BingImageCrawler
-import string
 
 def crawling():
     crawler = BingImageCrawler(storage={"root_dir": folder.get()})
-    crawler.crawl(keyword=word.get(), max_num=num.translate(table))
-
-#型の変換
-folder = StringVar()
-word = StringVar()
-num = IntVar()
-
-#数字の全半角変換
-table=str.maketrans("０１２３４５６７８９",string.digits)
-num
+    crawler.crawl(keyword=word.get(), max_num=num.get())
 
 root = Tk()
 root.title('画像収集')
+
+folder = StringVar()
+word = StringVar()
+num = IntVar()
 
 # ウィジェットの作成
 frame1 = ttk.Frame(root, padding=14)
@@ -28,7 +22,7 @@ entry1 = ttk.Entry(frame1, textvariable=folder)
 
 label3 = ttk.Label(frame1, text='検索語句')
 entry2 = ttk.Entry(frame1, textvariable=word)
-# todo:全角数字変換
+
 label4 = ttk.Label(frame1, text='収集枚数')
 entry3 = ttk.Entry(frame1, textvariable=num)
 
@@ -39,7 +33,8 @@ button1 = ttk.Button(
     command=crawling)
 # todo:配置方法の変更
 pac = [label1, label2, entry1, label3, entry2, label4, entry3, button1]
-# レイアウト
+#todo:終了時にポップアップ
+#レイアウト
 frame1.pack()
 for a in pac:
     a.pack(anchor=NW)
