f0efb10437c0b8bb3c257176d5b0173241691a3e
sin471 <tachiken.0414.ken@gmail.com>
2020-10-16 21:30:13 +0900

    tkcrawlを追加

--- crawler.py
+++
@@ -1,7 +0,0 @@
-from icrawler.builtin import BingImageCrawler
-
-file=input("Filename>>>")
-word=input("Keyword>>>")
-num=input("Num>>>")
-crawler = BingImageCrawler(storage={"root_dir": file})
-crawler.crawl(keyword=word ,max_num=int(num))
