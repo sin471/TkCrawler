29281828e5ab4f601fafa67532be58cd54c8a348
sin471 <tachiken.0414.ken@gmail.com>
2020-10-20 18:33:26 +0900

    保存先の指定を追加

--- crawler.py
+++ crawler.py
@@ -1,7 +1,11 @@
 from icrawler.builtin import BingImageCrawler
 
-file=input("Filename>>>")
+folder=input("Foldername>>>")
 word=input("Keyword>>>")
 num=input("Num>>>")
-crawler = BingImageCrawler(storage={"root_dir": file})
+path=input("Folder Path>>>")
+
+dest=path+"\\"+folder
+
+crawler = BingImageCrawler(storage={"root_dir": dest})
 crawler.crawl(keyword=word ,max_num=int(num))
