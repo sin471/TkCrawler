2b07dc9716fd6aecf4550d4bdb6c02986301bc81
sin471 <tachiken.0414.ken@gmail.com>
2020-10-21 19:33:01 +0900

    配置方向の変更

--- tkcrawler.py
+++ tkcrawler.py
@@ -5,15 +5,16 @@ import tkinter.messagebox as tkmsg
 from icrawler.builtin import BingImageCrawler
 from tkinter import filedialog
 
+# クロール
+
 
 def crawling():
     crawler = BingImageCrawler(storage={"root_dir": path.get()})
     crawler.crawl(keyword=word.get(), max_num=num.get())
     tkmsg.showinfo("", "完了しました")
 
+
 # フォルダー参照
-
-
 def folder_select():
     idir = 'C:\\'
     folder_path = dia.askdirectory(initialdir=idir)
@@ -21,59 +22,50 @@ def folder_select():
     entry_path.insert(0, folder_path)  # パスの表示
 
 
-
+# ウィジェットの作成
 root = Tk()
 root.title('TkCrawler')
 
-
+# 型を定義
 word = StringVar()
 num = IntVar()
 path = StringVar()
 
-# ウィジェットの作成
-frame1 = ttk.Frame(root, padding=14)
 
 style = ttk.Style()
 style.configure(".", font=("メイリオ", 10))
 
 
+frame1 = ttk.Frame(root, padding=14)
+frame1.grid(row=1, columnspan=3)
+
+
 label_word = ttk.Label(frame1, text='検索語句')
+label_word.grid(row=2, columnspan=3)
+
 entry_word = ttk.Entry(frame1, textvariable=word, width=22, font=("メイリオ", 10))
+entry_word.grid(row=3, columnspan=3)
 
 
 label_num = ttk.Label(frame1, text='収集枚数(半角数字)')
+label_num.grid(row=4, columnspan=3)
 
-spin_num = ttk.Spinbox(
-    frame1,
-    format='%1.0f',
-    textvariable=num,
-    from_=0,
-    to=1000,
-    width=20,
-    font=("メイリオ", 10))
-
+spin_num = ttk.Spinbox(frame1, format='%1.0f', textvariable=num,
+                       from_=0, to=1000, width=20, font=("メイリオ", 10))
+spin_num.grid(row=5, columnspan=3)
 
 
 label_path = ttk.Label(frame1, text='フォルダの保存先')
+label_path.grid(row=6, columnspan=3)
+
 entry_path = ttk.Entry(frame1, width=14, font=("メイリオ", 10), textvariable=path)
-button_path = ttk.Button(frame1, text="参照", width=6, command=folder_select)
-run_button = ttk.Button(
-    frame1,
-    text='実行',
-    command=crawling,
-    width=18)
-
-widget = [frame1, label_word, entry_word,
-          label_num, spin_num, label_path]
-
-# レイアウト
-for a in widget:
-    a.grid(row=widget.index(a), columnspan=3)
-
 entry_path.grid(columnspan=3, row=8, sticky=W)
+
+button_path = ttk.Button(frame1, text="参照", width=6, command=folder_select)
 button_path.grid(column=2, row=8, sticky=E)
 
-run_button.grid(columnspan=3, row=9)
+run_button = ttk.Button(frame1, text='実行', command=crawling, width=18)
+run_button.grid(row=9, columnspan=3)
 
 # ウィンドウの表示開始
 root.mainloop()
