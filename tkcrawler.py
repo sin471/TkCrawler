cd60942b4fc3bb37a13bd6d4e30ec3bd29559476
sin471 <tachiken.0414.ken@gmail.com>
2020-10-21 19:15:05 +0900

    フォルダ指定機能を追加

--- tkcrawler.py
+++ tkcrawler.py
@@ -1,58 +1,83 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog as dia
 import tkinter.messagebox as tkmsg
 from icrawler.builtin import BingImageCrawler
+from tkinter import filedialog
 
 
 def crawling():
-    crawler = BingImageCrawler(storage={"root_dir": folder.get()})
+    crawler = BingImageCrawler(storage={"root_dir": path.get()})
     crawler.crawl(keyword=word.get(), max_num=num.get())
     tkmsg.showinfo("", "完了しました")
 
+# フォルダー参照
+
+
+def folder_select():
+    idir = 'C:\\'
+    folder_path = dia.askdirectory(initialdir=idir)
+    entry_path.delete(0, END)
+    entry_path.insert(0, folder_path)  # パスの表示
+    #make_folder = folder_path+"\\"+str(folder.get)
+
 
 root = Tk()
-root.title('画像収集')
+root.title('TkCrawler')
 
-folder = StringVar()
+#folder = StringVar()
 word = StringVar()
 num = IntVar()
+path = StringVar()
 
 # ウィジェットの作成
 frame1 = ttk.Frame(root, padding=14)
 
-font = ("メイリオ", 10)
 style = ttk.Style()
-style.configure(".", font=font)
+style.configure(".", font=("メイリオ", 10))
 
-label1 = ttk.Label(frame1, text='自動でフォルダを作成し\nそこに画像を保存します')
-label2 = ttk.Label(frame1, text='フォルダ名')
-entry1 = ttk.Entry(frame1, textvariable=folder, width=22, font=font)
 
-label3 = ttk.Label(frame1, text='検索語句')
-entry2 = ttk.Entry(frame1, textvariable=word, width=22, font=font)
+label_word = ttk.Label(frame1, text='検索語句')
+entry_word = ttk.Entry(frame1, textvariable=word, width=22, font=("メイリオ", 10))
 
-label4 = ttk.Label(frame1, text='収集枚数(半角数字)')
 
-spin1 = ttk.Spinbox(
+label_num = ttk.Label(frame1, text='収集枚数(半角数字)')
+
+spin_num = ttk.Spinbox(
     frame1,
     format='%1.0f',
     textvariable=num,
     from_=0,
     to=1000,
     width=20,
-    font=font)
+    font=("メイリオ", 10))
 
-button1 = ttk.Button(
+"""label_foldername = ttk.Label(frame1, text='フォルダ名')
+entry_foldername = ttk.Entry(
+    frame1, textvariable=folder, width=22, font=("メイリオ", 10))"""
+
+
+# todo:word folder_select 変数
+label_path = ttk.Label(frame1, text='フォルダの保存先')
+entry_path = ttk.Entry(frame1, width=14, font=("メイリオ", 10), textvariable=path)
+button_path = ttk.Button(frame1, text="参照", width=6, command=folder_select)
+run_button = ttk.Button(
     frame1,
     text='実行',
-    command=crawling)
+    command=crawling,
+    width=18)
 
-widget = [frame1, label1, label2, entry1,
-          label3, entry2, label4, spin1, button1]
+widget = [frame1, label_word, entry_word,
+          label_num, spin_num, label_path]
 
 # レイアウト
 for a in widget:
-    a.grid(row=widget.index(a))
+    a.grid(row=widget.index(a), columnspan=3)
+
+entry_path.grid(columnspan=3, row=8, sticky=W)
+button_path.grid(column=2, row=8, sticky=E)
+
+run_button.grid(columnspan=3, row=9)
 
 # ウィンドウの表示開始
 root.mainloop()
